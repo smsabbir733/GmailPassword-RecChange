3da3bed7f984afd4941fa570c0c7ffba96a08c10
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-10-06 17:07:10 +0600

    Update : GmailPassword&RecChange.py "Test in Subbir Device"

--- GmailPassword&RecChange.py
+++ GmailPassword&RecChange.py
@@ -1,13 +1,30 @@
+"""
+We are using selenium documentation to buield this softwere
+https://selenium-python.readthedocs.io/
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("chromedriver.exe")
+
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--incognito")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument('disable-infobars')
+
+driver = webdriver.Chrome("chromedriver.exe", options = chrome_options)
+
 driver.get("http://gmail.com")
-assert "Python" in driver.title
-elem = driver.find_element(By.id="identifierId", "q")
-elem.clear()
-elem.send_keys("sabbir")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+# print(input("Stop :"))
+
+elem = driver.find_element(By.ID, "identifierId")
+
+elem.send_keys("smsabbir6001@gmail.com")
+
+next_button = driver.find_element(By.ID, "identifierNext")
+next_button.click()
